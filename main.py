ca8b0e39b7614afae13fd4e0664e7d43ce05e605
alejandroloda <alejandroloda@gmail.com>
2021-01-11 21:20:26 +0000

    file explorer como dock

--- main.py
+++ main.py
@@ -8,42 +8,39 @@
 #                                              |___/
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QMainWindow, QPushButton, QTextEdit, QHBoxLayout, \
-    QLineEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QMainWindow, QPushButton, QHBoxLayout, \
+    QLineEdit, QDockWidget, QTableView
+
+from PyQt5.QtCore import Qt
 
 
 class FileSelector(QWidget):
     def __init__(self):
         super().__init__()
-        self.title = 'PyQt5 file dialogs - pythonspot.com'
-        self.left = 100  # Donde empieza desde la izq
-        self.top = 100  # Donde empieza desde arriba
-        self.width = 640  # Cuanto mide horizontal
-        self.height = 480  # Cuanto mide vertical
-
         self.file = ""
-
         self.initUI()
 
     def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-
-
         hbox = QHBoxLayout()
         self.text_file_explorer = QLineEdit(self)
         self.text_file_explorer.setReadOnly(True)
         hbox.addWidget(self.text_file_explorer)
+
         self.button_file_explorer = QPushButton('Elegir archivo', self)
         self.button_file_explorer.clicked.connect(self.openFileNameDialog)
         hbox.addWidget(self.button_file_explorer)
+
+        self.button_refresh = QPushButton('Aplicar', self)
+        # TODO
+        # self.button_refresh.clicked.connect(self.openFileNameDialog)
+        hbox.addWidget(self.button_refresh)
 
         self.setLayout(hbox)
 
     def openFileNameDialog(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        filename, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "",
+        filename, _ = QFileDialog.getOpenFileName(self, "Elegir CSV", "",
                                                   "CSV Files (*.csv);;All Files (*)", options=options)
         if filename:
             self.file = filename
@@ -58,8 +55,16 @@ class MyMainWindow(QMainWindow):
         self.geometry = (300, 300, 1000, 500)
         self.setGeometry(*self.geometry)
 
+        self.dockWidget_file_explorer = QDockWidget('Files List', self)
+        self.dockWidget_file_explorer.setFloating(False)
+        # # Crear tree y asignar
         self.file_explorer = FileSelector()
-        self.setCentralWidget(self.file_explorer)
+        self.dockWidget_file_explorer.setWidget(self.file_explorer)
+        self.addDockWidget(Qt.TopDockWidgetArea, self.dockWidget_file_explorer)
+
+        main_widget = FileSelector()
+        self.setCentralWidget(main_widget)
+
 
 if __name__ == '__main__':
     app = QApplication([])
