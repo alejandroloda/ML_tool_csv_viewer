b7b51ebd82402c75cf074cce08f76350d44c462e
alejandroloda <alejandroloda@gmail.com>
2021-01-11 22:17:55 +0000

    basic csv reader with fixed name

--- main.py
+++ main.py
@@ -6,18 +6,22 @@
 # |_|  |_| \__,_||_||_| |_||_|    |_|   \___/  \__, ||_|   \__,_||_| |_| |_|
 #                                               __/ |
 #                                              |___/
-
+import csv
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QMainWindow, QPushButton, QHBoxLayout, \
-    QLineEdit, QDockWidget, QTableView
 
-from PyQt5.QtCore import Qt
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QMainWindow, QPushButton, QHBoxLayout, \
+    QLineEdit, QDockWidget, QTableView, QVBoxLayout
+
+from PyQt5.QtCore import Qt, QAbstractTableModel
+
 
 
 class FileSelector(QWidget):
-    def __init__(self):
+    def __init__(self, main):
         super().__init__()
         self.file = ""
+        self.main = main
         self.initUI()
 
     def initUI(self):
@@ -48,6 +52,25 @@ class FileSelector(QWidget):
             print(filename)
 
 
+class PandasModel(QAbstractTableModel):
+    def __init__(self, data, parent=None):
+        QAbstractTableModel.__init__(self, parent)
+        self._data = data
+
+    def rowCount(self, parent=None):
+        return len(self._data.values)
+
+    def columnCount(self, parent=None):
+        return self._data.columns.size
+
+    def data(self, index, role=Qt.DisplayRole):
+        if index.isValid():
+            if role == Qt.DisplayRole:
+                return QtCore.QVariant(str(
+                    self._data.values[index.row()][index.column()]))
+        return QtCore.QVariant()
+
+
 class MyMainWindow(QMainWindow):
     def __init__(self, window_title):
         super().__init__()
@@ -55,15 +78,24 @@ class MyMainWindow(QMainWindow):
         self.geometry = (300, 300, 1000, 500)
         self.setGeometry(*self.geometry)
 
-        self.dockWidget_file_explorer = QDockWidget('Files List', self)
+        # File explorer
+        self.dockWidget_file_explorer = QDockWidget(self)
         self.dockWidget_file_explorer.setFloating(False)
-        # # Crear tree y asignar
-        self.file_explorer = FileSelector()
+        self.file_explorer = FileSelector(self)
         self.dockWidget_file_explorer.setWidget(self.file_explorer)
         self.addDockWidget(Qt.TopDockWidgetArea, self.dockWidget_file_explorer)
 
-        main_widget = FileSelector()
-        self.setCentralWidget(main_widget)
+        # Reader
+        table = QTableView()
+
+        import pandas as pd
+        df = pd.read_csv("test.csv", sep=';')
+        print(df)
+        model = PandasModel(df)
+        table.setModel(model)
+    # self.table_view = MyWindow("test.csv")
+        #
+        self.setCentralWidget(table)
 
 
 if __name__ == '__main__':
